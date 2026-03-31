0580f656286f6b18803d36bb86c09f849875ca33
JGoslant-CS <je613333@ucf.edu>
2026-03-31 14:26:01 -0400

    Added Snake page and route path to snake page

--- arcade-frontend/src/App.tsx
+++ arcade-frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
+import SnakePage from './pages/SnakePage';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/snake" element={<SnakePage />} />
         </Routes>
       </Router>
     </AuthProvider>
